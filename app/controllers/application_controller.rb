057a97681d1447867632b9e4ce3d23f97a126d6f
mrapple <tony@oc.tc>
2015-08-11 13:46:45 -0700

    Support :rainbow:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_filter :pride
 
     def index
         @media = TonyB::Application.config.media.shuffle
         @rots  = TonyB::Application.config.rots
+    end
+
+    private
+    def pride
+        if params.has_key?(:pride)
+            session[:pride] = Time.now
+            return redirect_to root_path
+        else
+            session[:pride] = nil if session[:pride] && session[:pride] < 1.hour.ago
+            @pride = true if session[:pride]
+        end
     end
 end
