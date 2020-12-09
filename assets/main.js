d81a2e5118447c0ddc67d7853b9a4087ddb3ad4d
Tony <tony@oc.tc>
2020-12-08 16:49:57 -0800

    Spruce 'er up!

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    var tooltips = ["top", "bottom", "left", "right"]
-    for(var t in tooltips) {
-        var tooltip = tooltips[t];
-        $("[rel=tt-" + tooltip + "]").tooltip({
-            placement : tooltip,
-            container : "body"
-        });
-    }
-
     query = window.location.search
     if (query.startsWith('?pride')) {
         window.history.replaceState({}, document.title, "/");
